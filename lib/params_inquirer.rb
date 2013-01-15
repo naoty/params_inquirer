6be8900a830354dbb4496855b88aad6b266d1c30
naoty <naoty.k@gmail.com>
2013-01-15 23:25:35 +0900

    Generate inquirer when assigning value

--- lib/params_inquirer.rb
+++ lib/params_inquirer.rb
@@ -1,5 +1,17 @@
-require "params_inquirer/version"
+require 'active_support/core_ext/hash/indifferent_access'
+require 'active_support/string_inquirer'
 
 module ParamsInquirer
-  # Your code goes here...
+  class Parameters < ActiveSupport::HashWithIndifferentAccess
+    def []=(key, value)
+      super(key, convert_to_inquirer(value))
+    end
+
+    protected
+
+    def convert_to_inquirer(value)
+      s_value = value.is_a?(Symbol) ? value.to_s : value
+      ActiveSupport::StringInquirer.new(s_value)
+    end
+  end
 end
