0c1918f90aeeb2f6a2f27723176181d0a37c79c1
naoty <naoty.k@gmail.com>
2013-01-20 00:51:26 +0900

    Load railtie during boot

--- lib/params_inquirer/action_controller/base.rb
+++ lib/params_inquirer/action_controller/base.rb
@@ -1,5 +1,5 @@
 require 'active_support/core_ext/module/attr_internal'
-require 'params_inquirer'
+require 'params_inquirer/parameters'
 
 module ParamsInquirer
   module ActionController::Base
---
+++ lib/params_inquirer/parameters.rb
@@ -0,0 +1,24 @@
+require 'active_support/core_ext/hash/indifferent_access'
+require 'active_support/string_inquirer'
+
+module ParamsInquirer
+  class Parameters < ActiveSupport::HashWithIndifferentAccess
+    def []=(key, value)
+      super(key, convert_value(value))
+    end
+
+    protected
+
+    def convert_value(value)
+      if value.is_a?(String)
+        ActiveSupport::StringInquirer.new(value)
+      elsif value.is_a?(Symbol)
+        ActiveSupport::StringInquirer.new(value.to_s)
+      elsif value.is_a?(Hash)
+        Parameters.new(value)
+      else
+        value
+      end
+    end
+  end
+end
