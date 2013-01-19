9d9c9022b9b7e8cdb69872603e03c62df85e9c18
naoty <naoty.k@gmail.com>
2013-01-20 00:59:13 +0900

    Fix bug

--- lib/params_inquirer/action_controller/base.rb
+++ lib/params_inquirer/action_controller/base.rb
@@ -2,11 +2,13 @@ require 'active_support/core_ext/module/attr_internal'
 require 'params_inquirer/parameters'
 
 module ParamsInquirer
-  module ActionController::Base
-    attr_internal :params
+  module ActionController
+    module Base
+      attr_internal :params
 
-    def params
-      @_params ||= ParamsInquirer::Parameters.new(super)
+      def params
+        @_params ||= ParamsInquirer::Parameters.new(super)
+      end
     end
   end
 end
