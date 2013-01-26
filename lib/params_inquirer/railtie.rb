dccbabaa6fa1d6dad249784d426abee1c3ab3cf1
naoty <naoty.k@gmail.com>
2013-01-26 23:35:41 +0900

    Lazily load ParamsInquirer with ActiveSupport.on_load

--- lib/params_inquirer/railtie.rb
+++ lib/params_inquirer/railtie.rb
@@ -1,9 +1,12 @@
+require 'active_support/lazy_load_hooks'
 require 'params_inquirer/action_controller/base'
 
 module ParamsInquirer
   class Railtie < ::Rails::Railtie
     initializer 'initialize params_inquirer' do
-      ::ActionController::Base.send :include, ParamsInquirer::ActionController::Base
+      ActiveSupport.on_load(:action_controller) do
+        ::ActionController::Base.send :include, ParamsInquirer::ActionController::Base
+      end
     end
   end
 end
